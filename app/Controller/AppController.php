48c5b775df2e8e3d8732a58021698d2af020da9d
spout <spoutnik@gmail.com>
2012-10-21 14:46:51 +0200

    Fix indent

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -157,43 +157,43 @@ abstract class AppController extends Controller {
     * @throws PrivateActionException When actions are not public or prefixed by _
     * @throws MissingActionException When actions are not defined and scaffolding and CRUD is not enabled.
     */
-    public function invokeAction(CakeRequest $request) {
-        try {
-            return parent::invokeAction($request);
-        } catch (MissingActionException $e) {
-            // Check for any dispatch components
-            if (!empty($this->dispatchComponents)) {
-                // Iterate dispatchComponents
-                foreach ($this->dispatchComponents as $component => $enabled) {
-                    // Skip them if they aren't enabled
-                    if (empty($enabled)) {
-                        continue;
-                    }
+	public function invokeAction(CakeRequest $request) {
+		try {
+			return parent::invokeAction($request);
+		} catch (MissingActionException $e) {
+			// Check for any dispatch components
+			if (!empty($this->dispatchComponents)) {
+				// Iterate dispatchComponents
+				foreach ($this->dispatchComponents as $component => $enabled) {
+					// Skip them if they aren't enabled
+					if (empty($enabled)) {
+						continue;
+					}
 
-                    // Skip if isActionMapped isn't defined in the Component
-                    if (!method_exists($this->{$component}, 'isActionMapped')) {
-                        continue;
-                    }
+					// Skip if isActionMapped isn't defined in the Component
+					if (!method_exists($this->{$component}, 'isActionMapped')) {
+						continue;
+					}
 
-                    // Skip if the action isn't mapped
-                    if (!$this->{$component}->isActionMapped($request->params['action'])) {
-                        continue;
-                    }
+					// Skip if the action isn't mapped
+					if (!$this->{$component}->isActionMapped($request->params['action'])) {
+						continue;
+					}
 
-                    // Skip if executeAction isn't defined in the Component
-                    if (!method_exists($this->{$component}, 'executeAction')) {
-                        continue;
-                    }
-					
-                    // Execute the callback, should return CakeResponse object
-                    return $this->{$component}->executeAction();
-                }
-            }
+					// Skip if executeAction isn't defined in the Component
+					if (!method_exists($this->{$component}, 'executeAction')) {
+						continue;
+					}
 
-            // No additional callbacks, re-throw the normal Cake exception
-            throw $e;
-        }
-    }
+					// Execute the callback, should return CakeResponse object
+					return $this->{$component}->executeAction();
+				}
+			}
+
+			// No additional callbacks, re-throw the normal Cake exception
+			throw $e;
+		}
+	}
 	
 	/**
 	* Set language and locale via the URL
